aa36c21810748bf23ad335c119285605d10aa358
ClownYH <qwer1745@gmail.com>
2023-11-15 18:11:09 +0900

    add db for jdbc lecture

---
+++ General/Scripts/JDBC chap02 understand.sql
@@ -0,0 +1,37 @@
+SELECT EMP_ID, EMP_NAME FROM EMPLOYEE;
+
+SELECT * FROM employee;
+
+SELECT
+	emp_no,
+	emp_name,
+	phone
+FROM
+	employee 
+ORDER BY
+	SALARY DESC
+LIMIT 1;
+
+SELECT * FROM job;
+	
+SELECT 
+	emp_no,
+	emp_name,
+	phone,
+	job_name
+FROM
+	employee 
+JOIN
+	job ON job.JOB_CODE = employee.JOB_CODE 
+WHERE
+	emp_name = "선동일";
+	
+SHOW TABLEs;
+
+SELECT * FROM department ;
+
+SELECT * FROM location ;
+
+SELECT * FROM nation ;
+
+SELECT * FROM sal_grade ;
